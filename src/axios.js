d8cd08bfa9ac30152890b6d8110f40befcad5a55
kffl <pawel@kuffel.io>
2020-04-29 17:40:23 +0200

    add env file

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+console.log(process.env);
 export const api = axios.create({
-	baseURL: 'http://localhost:8080/api/',
+	baseURL: process.env.VUE_APP_BACKEND_ADDR,
 	headers: {
 		'content-type': 'application/json',
 	},
