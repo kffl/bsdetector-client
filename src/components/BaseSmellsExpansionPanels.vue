f276af634af7451687fbf260021cbb29722a6b51
kffl <pawel@kuffel.io>
2020-05-03 21:44:06 +0200

    add knowledge base navigation, add kb links in results

--- src/components/BaseSmellsExpansionPanels.vue
+++ src/components/BaseSmellsExpansionPanels.vue
@@ -4,9 +4,11 @@
 	v-expansion-panels(v-if='detectedSmells.length', multiple, hover, :value='expandedPanels')
 		v-expansion-panel(v-for='(smell, i) in detectedSmells', :key='i')
 			v-expansion-panel-header(color='grey lighten-5', ripple)
-				span(class='panel-header subtitle-1 font-weight-medium') {{ smell.smellName }}
+				span(class='panel-header subtitle-1 font-weight-medium') {{ smell.smellMeta.title }}
 			v-expansion-panel-content
-				p(class='mt-2 mb-4 grey--text text--darken-1') {{ smell.smellDescription }}
+				p(class='mt-2 mb-4 grey--text text--darken-1') {{ smell.smellMeta.description + " " }}
+				p(class='mt-2 mb-4 grey--text text--darken-1')
+					router-link(:to="'/knowledge-base/' + smell.smellMeta.slug") Learn more abouth this smell
 				h6(class='title font-weight-regular') Occurrences
 				v-card(v-for='(occurrence, j) in smell.occurrences', :key='j', class='occurrence-container grey lighten-4 px-2', outlined, tile)
 					code(class='occurrence-snippet font-weight-medium') {{ occurrence.snippet }}
@@ -16,6 +18,8 @@
 </template>
 
 <script>
+import smells from '../resources/smells';
+
 export default {
 	name: 'BaseSmellsExpansionPanels',
 
@@ -32,7 +36,8 @@ export default {
 
 	computed: {
 		detectedSmells: function () {
-			return this.detectorResult ? this.detectorResult.smellsDetected.filter((smell) => smell.occurrences.length) : [];
+			var result = this.detectorResult ? this.detectorResult.smellsDetected.filter((smell) => smell.occurrences.length) : [];
+			return result.map((smell) => ({ ...smell, smellMeta: smells.find(el => el.name === smell.smellName) }));
 		},
 		expandedPanels: function () {
 			return [...Array(this.detectedSmells.length).keys()];
