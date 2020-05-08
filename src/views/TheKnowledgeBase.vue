8003081d0c25ae228fa401d0bbae80be7bda21ae
kffl <pawel@kuffel.io>
2020-05-08 13:46:19 +0200

    knowledge base fixes

--- src/views/TheKnowledgeBase.vue
+++ src/views/TheKnowledgeBase.vue
@@ -1,41 +1,41 @@
 <template>
   <div class="outer-container">
-    <v-navigation-drawer width="400">
+    <v-navigation-drawer :permanent=true width="400">
       <v-list dense nav class="py-0">
-        <v-list-item two-line :class="miniVariant && 'px-0'">
-          <v-list-item-content>
-            <v-list-item-title class="list-header">Knowledge Base</v-list-item-title>
-            <v-list-item-subtitle>Available smells</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
+			<v-list-item two-line >
+				<v-list-item-content>
+					<v-list-item-title class="list-header">Knowledge Base</v-list-item-title>
+					<v-list-item-subtitle>Available smells</v-list-item-subtitle>
+				</v-list-item-content>
+			</v-list-item>
 
-        <v-divider></v-divider>
+			<v-divider></v-divider>
 
-        <v-list-item v-for="item in navSmells" :key="item.title" link :to='"/knowledge-base/" + item.slug'>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-container class="content">
-      <p v-if="loading">
-        <v-progress-circular indeterminate color="red"></v-progress-circular>
-      </p>
-      <div v-if="error">
-        <h2>404</h2>
-        <p>The requested page was not found</p>
-        <v-btn to="/">Go back</v-btn>
-      </div>
-      <div v-if="content">
-        <vue-markdown v-bind:source="content" />
-      </div>
-			<div v-if="mainPage">
-				<h1>Knowledge base</h1>
-				<p>Welcome to the knowledge base. Here you can find detailed explenations of code smells that are being detected in your code.</p>
-			</div>
-    </v-container>
-  </div>
+			<v-list-item v-for="item in navSmells" :key="item.title" link :to='"/knowledge-base/" + item.slug'>
+				<v-list-item-content>
+					<v-list-item-title>{{ item.title }}</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
+		</v-list>
+	</v-navigation-drawer>
+	<v-container class="content">
+		<p v-show="loading">
+			<v-progress-circular indeterminate color="red"></v-progress-circular>
+		</p>
+		<div v-if="error">
+			<h2>404</h2>
+			<p>The requested page was not found</p>
+			<v-btn to="/">Go back</v-btn>
+		</div>
+		<div v-else-if="content">
+			<vue-markdown v-bind:source="content" />
+		</div>
+		<div v-if="mainPage && !loading">
+			<h1>Knowledge base</h1>
+			<p>Welcome to the knowledge base. Here you can find detailed explenations of code smells that are being detected in your code.</p>
+		</div>
+	</v-container>
+</div>
 </template>
 
 <script>
@@ -75,18 +75,16 @@ export default {
 		},
 		fetchPageContent () {
 			this.error = this.content = null;
+			this.mainPage = false;
 			this.loading = true;
 			fetchContent(this.$route.params.slug)
-				.get('')
 				.then(res => {
 					this.loading = false;
 					this.content = res.data;
-					this.mainPage = false;
 				})
 				.catch(err => {
 					this.loading = false;
 					this.error = err;
-					this.mainPage = false;
 				});
 		},
 	},
@@ -98,7 +96,7 @@ export default {
 
 <style scoped>
 	.outer-container {
-		padding: 0 !important;
+		padding: 0;
 		height: 100%;
 		display: flex;
 	}
@@ -106,16 +104,16 @@ export default {
 		padding: 32px;
 	}
 	.list-header {
-		font-size: 1.6em !important;
-		line-height: 1.6em !important;
+		font-size: 1.6em;
+		line-height: 1.6em;
 	}
 </style>
 <style>
 	.content code::after, .content code::before {
-		content: none !important;
+		content: none;
 	}
 	.content code, .content code{
-		padding: 8px !important;
+		padding: 8px;
 		margin-top: 10px;
 		width: 100%;
 	}
