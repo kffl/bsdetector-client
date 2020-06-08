f8c45a755fd99a2f432867b59cdaeba5dc525489
tomasz-pucka <tomasz.pucka2@gmail.com>
2020-06-08 19:19:50 +0200

    add statistics in SmellsExpansionPanels

--- src/components/BaseFilesSmellsExpansionPanels.vue
+++ src/components/BaseFilesSmellsExpansionPanels.vue
@@ -1,17 +1,21 @@
 <template lang='pug'>
-	v-expansion-panels(multiple, hover, :value='expandedPanels')
-		v-expansion-panel(
-			v-for='(fileSmells, i) in detectorResult',
-			:key='i',
-			:class="{ 'file-expansion-panel': i < detectorResult.length - 1}",
-			)
-			v-expansion-panel-header(color='grey lighten-4', ripple)
-				span(class='panel-header title font-weight-medium') {{ fileSmells.fileName }}
-			v-expansion-panel-content(class='pt-4')
-				p(v-if="fileSmells.error", class='title font-weight-regular ma-0')
-					| Error! {{ fileSmells.error.message ? fileSmells.error.message : 'Code smells detection failed.' }}.
-				base-smells-expansion-panels(v-else, :detectorResult='fileSmells', :use-container-for-clean-code='false')
-
+	div
+		p(class='body-2 mb-1 grey--text text--darken-5').
+			Total number of detected code smells - {{ totalNumberOfDetectedSmells }},
+			files analyzed - {{ detectorResult.length }},
+			lines analyzed - {{ totalNumberOfAnalyzedLines }}
+		v-expansion-panels(multiple, hover, :value='expandedPanels')
+			v-expansion-panel(
+				v-for='(fileSmells, i) in detectorResult',
+				:key='i',
+				:class="{ 'file-expansion-panel': i < detectorResult.length - 1}",
+				)
+				v-expansion-panel-header(color='grey lighten-4', ripple)
+					span(class='panel-header title font-weight-medium') {{ fileSmells.fileName }}
+				v-expansion-panel-content(class='py-2')
+					p(v-if="fileSmells.error", class='title font-weight-regular ma-0')
+						| Error! {{ fileSmells.error.message ? fileSmells.error.message : 'Code smells detection failed.' }}.
+					base-smells-expansion-panels(v-else, :detectorResult='fileSmells', :use-container-for-clean-code='false')
 </template>
 
 <script>
@@ -26,6 +30,15 @@ export default {
 	},
 
 	computed: {
+		totalNumberOfDetectedSmells: function () {
+			return this.detectorResult.reduce(
+				(totalNoSmells, fileSmells) =>
+					fileSmells.smellsDetected.reduce((fileNoSmells, smells) => smells.occurrences.length + fileNoSmells, 0) + totalNoSmells, 0,
+			);
+		},
+		totalNumberOfAnalyzedLines: function () {
+			return this.detectorResult.reduce((acc, fileSmells) => fileSmells.linesAnalyzed + acc, 0);
+		},
 		expandedPanels: function () {
 			return [...Array(this.detectorResult.length).keys()];
 		},
--- src/components/BaseSmellsExpansionPanels.vue
+++ src/components/BaseSmellsExpansionPanels.vue
@@ -1,25 +1,29 @@
 <template lang='pug'>
-- const cleanCodeMessage = 'No smells detected!';
-
-	v-expansion-panels(v-if='detectedSmells.length', multiple, hover, :value='expandedPanels')
-		v-expansion-panel(v-for='(smell, i) in detectedSmells', :key='i')
-			v-expansion-panel-header(color='grey lighten-5', ripple)
-				span(class='panel-header subtitle-1 font-weight-medium') {{ smell.smellMeta.title }}
-			v-expansion-panel-content
-				p(class='mt-2 mb-2 grey--text text--darken-1') {{ smell.smellMeta.description }}
-				router-link(:to='`/knowledge-base/${smell.smellMeta.slug}`', target='_blank') Learn more about this smell
-				h6(class='title font-weight-regular mt-3') Occurrences
-				codemirror(
-					ref='codeMirrorSnippets',
-					v-for='(occurrence, j) in smell.occurrences',
-					:key='j',
-					class='smells-code-mirror',
-					:value='occurrence.snippet',
-					:options='snippetCodeMirrorOptions'
-					)
-	v-card(v-else-if='!!detectorResult && useContainerForCleanCode')
-		v-card-text(class='title font-weight-regular text--primary')= cleanCodeMessage
-	p(v-else-if='!!detectorResult', class='title font-weight-regular ma-0')= cleanCodeMessage
+- const cleanCodeMessage = 'No code smells detected! Your code is clean.';
+	div
+		p(class='body-2 grey--text text--darken-5', :class="numberOfDetectedSmells ? 'mb-2' : 'mb-0'")
+			template(v-if='numberOfDetectedSmells')
+				| Number of detected code smells - {{ numberOfDetectedSmells }}, lines analyzed - {{ detectorResult.linesAnalyzed }}
+			template(v-else) Number of analyzed lines - {{ detectorResult.linesAnalyzed }}
+		v-expansion-panels(v-if='detectedSmells.length', multiple, hover, :value='expandedPanels')
+			v-expansion-panel(v-for='(smell, i) in detectedSmells', :key='i')
+				v-expansion-panel-header(color='grey lighten-5', ripple)
+					span(class='panel-header subtitle-1 font-weight-medium') {{ smell.smellMeta.title }}
+				v-expansion-panel-content
+					p(class='mt-2 mb-2 grey--text text--darken-1') {{ smell.smellMeta.description }}
+					router-link(:to='`/knowledge-base/${smell.smellMeta.slug}`', target='_blank') Learn more about this smell.
+					h6(class='title font-weight-regular mt-3') Occurrences
+					codemirror(
+						ref='codeMirrorSnippets',
+						v-for='(occurrence, j) in smell.occurrences',
+						:key='j',
+						class='smells-code-mirror',
+						:value='occurrence.snippet',
+						:options='snippetCodeMirrorOptions'
+						)
+		v-card(v-else-if='!!detectorResult && useContainerForCleanCode')
+			v-card-text(class='title font-weight-regular text--primary')= cleanCodeMessage
+		p(v-else-if='!!detectorResult', class='title font-weight-regular mb-0 mt-3')= cleanCodeMessage
 </template>
 
 <script>
@@ -53,6 +57,9 @@ export default {
 		detectedSmells: function () {
 			var result = this.detectorResult ? this.detectorResult.smellsDetected.filter((smell) => smell.occurrences.length) : [];
 			return result.map((smell) => ({ ...smell, smellMeta: smells.find(el => el.name === smell.smellName) }));
+		},
+		numberOfDetectedSmells: function () {
+			return this.detectedSmells.reduce((acc, smell) => smell.occurrences.length + acc, 0);
 		},
 		expandedPanels: function () {
 			return [...Array(this.detectedSmells.length).keys()];
